0e788c54f5cbfe31f856f33004959a3c497c3a05
Ari Ayvazyan <ariay@live.at>
2015-01-08 22:50:27 +0100

    use the jetty plugin

--- src/main/java/org/loadbalancer/LoadBalancerRoute.java
+++ src/main/java/org/loadbalancer/LoadBalancerRoute.java
@@ -1,35 +1,28 @@
 package org.loadbalancer;
 
-import org.apache.camel.EndpointInject;
-import org.apache.camel.Expression;
 import org.apache.camel.builder.RouteBuilder;
-import org.apache.camel.component.mock.MockEndpoint;
 
 public class LoadBalancerRoute extends RouteBuilder {
 
 
     private String inputPort = "15000";
 
-    private static final String SERVER1_URL = "http://localhost:8080";
-    private static final String SERVER2_URL = "http://localhost:8081";
 
-
-    private String[] destinations={"http://www.google.com?bridgeEndpoint=true",
-            "http://www.bing.com?bridgeEndpoint=true",
-            "http://www.duckduckgo.com?bridgeEndpoint=true"};
+    private String[] destinations = {
+            "http://www.google.com:8080",
+            "http://www.bing.com:8080",
+            "http://www.duckduckgo.com:8080"
+    };
 
     @Override
-	public void configure() throws Exception {
-		//jetty:http://0.0.0.0:"+inputPort+"?matchOnUriPrefix=true"
-
-        destinations = new String[]{"{{output1}}", "{{output2}}"};
-
-        from("{{input}}").routeId("LOADBALANCER")
-		.to("log:org.loadbalancer.IN?showAll=true&multiline=true")
-		.loadBalance()
-		.roundRobin()
-                //.to(SERVER1_URL + "?bridgeEndpoint=true", SERVER2_URL + "?bridgeEndpoint=true");
-        .to(destinations);
-	}
+    public void configure() throws Exception {
+        from("jetty:http://localhost:8080/apache-camel-Load-Balancer/")
+                .routeId("LOADBALANCER")
+                .to("http://www.duckduckgo.com:8080")
+                        //.to("log:org.loadbalancer.IN?showAll=true&multiline=true")
+                .loadBalance()
+                .roundRobin()
+                .to(destinations);
+    }
 
 }
