300f7231052507161a7c312b49f7fa04fcf3965e
Ari Ayvazyan <ariay@live.at>
2015-01-14 21:58:00 +0100

    start two instances on port 8083, 8084 if no args are supplied

--- LoadBalancerClientServlet/src/main/java/server/Main.java
+++ LoadBalancerClientServlet/src/main/java/server/Main.java
@@ -4,17 +4,19 @@ import java.io.IOException;
 
 public class Main {
 	public static void main(final String[] args) throws IOException{
-		int port = 8080;
 		if (args.length > 0) {
+			int port = 8080;
 			try {
 				port = Integer.parseInt(args[0]);
 			} catch (NumberFormatException e) {
 				System.err.println("Argument" + args[0] + " must be an integer.");
 				System.err.println("Defaulting to 8080");
 			}
+			System.out.println("Starting Jetty ..");
+			new JettyEmbeddedRunner().startServer(port);
+		} else {
+			new JettyEmbeddedRunner().startServer(8083);
+			new JettyEmbeddedRunner().startServer(8084);
 		}
-
-		System.out.println("Starting Jetty ..");
-		new JettyEmbeddedRunner().startServer(port);
 	}
 }
